a6d02dcd3d43eac5cfc8d6322d93a6ed02193b7b
Edwin Lizama <lg20012@ues.edu.sv>
2024-09-21 20:54:33 -0600

    Movimiento a public

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/', 'resources/js/'],
+            input: ['public/css/', 'public/js/'],
             refresh: true,
         }),
     ],
